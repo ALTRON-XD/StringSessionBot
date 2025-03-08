7c724de71923d356315d2b7113ff7f287a66d856
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2025-03-08 21:07:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import env
 import logging
+import asyncio
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
-
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
@@ -13,20 +13,25 @@ app = Client(
     api_hash=env.API_HASH,
     bot_token=env.BOT_TOKEN,
     in_memory=True,
-    plugins={'root':'StringSessionBot'},
+    plugins={'root': 'StringSessionBot'},
 )
 
-
-if __name__ == "__main__":
+async def main():
     logging.info("Starting the bot")
     try:
-        app.start()
+        await app.start()  # Await the app.start() coroutine
     except (ApiIdInvalid, ApiIdPublishedFlood):
         raise Exception("Your API_ID/API_HASH is not valid.")
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
+    
     uname = "lisztomanic_string_bot"
     logging.info(f"@{uname} is now running!")
-    idle()
-    app.stop()
+    
+    await idle()  # Await the idle coroutine
+    await app.stop()  # Await the app.stop() coroutine
+    
     logging.info("Bot stopped. Alvida!")
+
+if __name__ == "__main__":
+    asyncio.run(main())  # Run the main coroutine in the event loop
